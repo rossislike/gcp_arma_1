eeb7a0590467c53640180702a60731ec52f74849
rossislike <ross.agginie@gmail.com>
2024-04-30 23:33:30 -0400

    Add task2

--- task2/main.tf
+++ task2/main.tf
@@ -11,7 +11,7 @@ provider "google" {
   # Configuration options
   region = "us-central1"
   project = "my-second-project-416401"
-  credentials = "${path.module}../my-second-project-416401-f024ec5b7771.json"
+  credentials = "${path.module}/../my-second-project-416401-f024ec5b7771.json"
 }
 
 
@@ -23,26 +23,151 @@ provider "google" {
 # }
 
 
-resource "google_compute_network" "auto-vpc-tf" {
+resource "google_compute_network" "gvpc" {
   name = "myvpc"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "sub-sg" {
+resource "google_compute_subnetwork" "gsubnet" {
   name ="sub-sg"
-  network = google_compute_network.auto-vpc-tf.id
+  network = google_compute_network.gvpc.id
   ip_cidr_range = "10.151.1.0/24"
   region = "us-central1"
 }
 
+resource "google_compute_firewall" "gfirewall_icmp" {
+  name    = "myvpc-allow-icmp"
+  network = google_compute_network.gvpc.name
 
-#resource "google_compute_network" "custom-vpc-tf" {
-  #name = "custom-vpc-tf"
- #auto_create_subnetworks = false
-#}
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  priority = 65534
+}
+
+resource "google_compute_firewall" "gfirewall_rdp" {
+  name    = "myvpc-allow-rdp"
+  network = google_compute_network.gvpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  priority = 65534
+
+}
+
+resource "google_compute_firewall" "gfirewall_ssh" {
+  network = google_compute_network.gvpc.name
+  name    = "myvpc-allow-ssh"
+  priority = 65534
+
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_firewall" "gfirewall_http" {
+  network = google_compute_network.gvpc.name
+  name    = "myvpc-allow-http"
+  priority = 65534
+
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+}
+
+resource "google_compute_firewall" "gfirewall_custom" {
+  network = google_compute_network.gvpc.name
+  name    = "myvpc-allow-custom"
+  priority = 65534
+
+  source_ranges = ["10.151.1.0/24"]
+
+  allow {
+    protocol = "all"
+  }
+}
+
+
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "instance-20240501-032003" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-20240501-032003"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240415"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-micro"
+
+  metadata = {
+    startup-script = "#Thanks to Remo\n#!/bin/bash\n# Update and install Apache2\napt update\napt install -y apache2\n\n# Start and enable Apache2\nsystemctl start apache2\nsystemctl enable apache2\n\n# GCP Metadata server base URL and header\nMETADATA_URL=\"http://metadata.google.internal/computeMetadata/v1\"\nMETADATA_FLAVOR_HEADER=\"Metadata-Flavor: Google\"\n\n# Use curl to fetch instance metadata\nlocal_ipv4=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/network-interfaces/0/ip\")\nzone=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/zone\")\nproject_id=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/project/project-id\")\nnetwork_tags=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/tags\")\n\n# Create a simple HTML page and include instance details\ncat <<EOF > /var/www/html/index.html\n<html><body>\n<h2>Welcome to your custom website.</h2>\n<h3>Created with a direct input startup script!</h3>\n<p><b>Instance Name:</b> $(hostname -f)</p>\n<p><b>Instance Private IP Address: </b> $local_ipv4</p>\n<p><b>Zone: </b> $zone</p>\n<p><b>Project ID:</b> $project_id</p>\n<p><b>Network Tags:</b> $network_tags</p>\n</body></html>\nEOF"
+  }
+
+  name = "instance-20240501-032003"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = google_compute_subnetwork.gsubnet.name
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "715004262141-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "us-central1-a"
+}
+
 
 output "auto" {
-  value = google_compute_network.auto-vpc-tf.id
+  value = google_compute_network.gvpc.id
 }
 
 #output "custom" {
